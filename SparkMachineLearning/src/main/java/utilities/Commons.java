087eb94fa42c9da7586cbb4c7dafa36b54e5e638
giovanni.rotondaro <rtngnn99b17c588u@studenti.unical.it>
2022-12-30 16:43:21 +0100

    AddGiovanniConfiguration

--- SparkMachineLearning/src/main/java/utilities/Commons.java
+++ SparkMachineLearning/src/main/java/utilities/Commons.java
@@ -1,10 +1,19 @@
 package utilities;
 
 public class Commons {
+    /*
+    //HASAN
     public static final String TRAIN_DATASET = "hdfs://master:9000/user/bigdata/project/input/DataSet_Train.csv";
     public static final String TEST_DATASET = "hdfs://master:9000/user/bigdata/project/input/DataSet_Test.csv";
     public static final String MAP_REDUCE_1 = "hdfs://master:9000/user/mahbubhasan/project/output/map-reduce-1/part-r-00000";
     public static final String MAP_REDUCE_2 = "hdfs://master:9000/user/mahbubhasan/project/output/map-reduce-2/part-r-00000";
     public static final String MAP_REDUCE_3 = "hdfs://master:9000/user/mahbubhasan/project/output/map-reduce-3/part-r-00000";
+*/
 
+    //GIOVANNI
+    public static final String TRAIN_DATASET = "project/input/DataSet_Train.csv";
+    public static final String TEST_DATASET = "project/input/DataSet_Test.csv";
+    public static final String MAP_REDUCE_1 = "project/output/map-reduce-1/part-r-00000";
+    public static final String MAP_REDUCE_2 = "project/output/map-reduce-2/part-r-00000";
+    public static final String MAP_REDUCE_3 = "project/output/map-reduce-3/part-r-00000";
 }
