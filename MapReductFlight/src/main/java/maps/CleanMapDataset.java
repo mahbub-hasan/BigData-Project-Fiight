077f560840eddd95c50df65bd1de7140786fa05c
mhasan <mridul.link@gmail.com>
2023-01-13 18:39:43 +0100

    map reduce problem solve

--- MapReductFlight/src/main/java/maps/CleanMapDataset.java
+++ MapReductFlight/src/main/java/maps/CleanMapDataset.java
@@ -5,13 +5,19 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import utilites.FileMapper;
 
 import java.io.IOException;
 
 public class CleanMapDataset extends Mapper<LongWritable, Text, Text, Text> {
     @Override
     protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, Text>.Context context) throws IOException, InterruptedException {
-        String st=value.toString();
-        context.write(new Text(st),new Text(""));
+        String[] data = value.toString().split(";");
+        if(data.length == FileMapper.TOTAL_LENGTH){
+            if(!data[0].equals("Airline")){
+                String st=value.toString();
+                context.write(new Text(st),new Text(""));
+            }
+        }
     }
 }
